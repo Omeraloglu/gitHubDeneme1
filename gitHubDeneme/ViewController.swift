9c59992fe168a6b966f42f892f286ffdc3f29848
omeraloglu <omeraloglu16@hotmail.com>
2019-05-31 10:57:29 +0300

    add disappear and labels

--- gitHubDeneme/ViewController.swift
+++ gitHubDeneme/ViewController.swift
@@ -11,10 +11,16 @@ import UIKit
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
-        super.viewDidLoad()
+    //    super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    override func viewWillAppear(_ animated: Bool) {
+        <#code#>
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        <#code#>
+    }
 }
 
