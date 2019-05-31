f4adf8777c060e7d9a1a5c8854c9d35c887e1083
omeraloglu <omeraloglu16@hotmail.com>
2019-05-31 10:52:56 +0300

    first Step

---
+++ gitHubDeneme/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  gitHubDeneme
+//
+//  Created by Ömer ALOĞLU on 31.05.2019.
+//  Copyright © 2019 Ömer ALOĞLU. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
